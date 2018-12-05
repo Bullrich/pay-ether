a6785a5c77a90a92a7dd8fda85545b1c74ec5d80
Javier Bullrich <javierbullrich@gmail.com>
2018-12-05 14:50:48 -0300

    Updates

---
+++ precache-manifest.1866bc7c8faa2392722bcfee1bc35eea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86383c74994499616d30",
+    "url": "/pay-ether/static/css/main.bce40508.chunk.css"
+  },
+  {
+    "revision": "86383c74994499616d30",
+    "url": "/pay-ether/static/js/main.86383c74.chunk.js"
+  },
+  {
+    "revision": "015898a8e5317a311746",
+    "url": "/pay-ether/static/js/1.015898a8.chunk.js"
+  },
+  {
+    "revision": "883244795f5ef5a9d73c",
+    "url": "/pay-ether/static/js/runtime~main.88324479.js"
+  },
+  {
+    "revision": "36222639a9ea77f66f43bbe8f6a6fd06",
+    "url": "/pay-ether/index.html"
+  }
+];
